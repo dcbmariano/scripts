516f1adf1edbf1e399b34551ae3da2c028e0339d
dcbmariano <diogohenks@hotmail.com>
2014-10-22 14:10:02 -0300

    v5 (update)

--- convertemultifasta.py
+++ convertemultifasta.py
@@ -1,13 +1,22 @@
+#! /usr/bin/python26
+#     Program: convertemultifasta.py
+#    Function: Converter arquivos multifasta em arquivo com uma unica sequencia
+# Description: Recebe um arquivo multifasta, elimina os cabecalhos e os substitui por 100 Ns
+#      Author: Diego Mariano
+#     Version: 1
+
 from Bio import SeqIO
 import sys
 
-w = open('result.fasta','w')
+w = open('scaffold.fasta','w')
 p = sys.argv[1]
+w.write('>scaffold')
 
 for i in SeqIO.parse(p,"fasta"):
 	seq = str(i.seq)
 	w.write(seq)
-	w.write('N')
+	w.write('NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN')
+	
 w.close()
 w.closed
 	
